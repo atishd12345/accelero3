96a026d958247065be00245601a781f587369d3f
Atish <atishd12345@gmail.com>
2015-11-01 23:16:37 +0400

    add working fragment to main activity

--- app/src/main/java/com/example/atish/accelero3/MainActivity.java
+++ app/src/main/java/com/example/atish/accelero3/MainActivity.java
@@ -1,29 +1,35 @@
 package com.example.atish.accelero3;
 
+import android.support.annotation.AnimRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.RelativeLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+       // RelativeLayout mainLayout = new RelativeLayout(this);
+
+
+
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+
         }
-    }
+
 
 
     @Override
@@ -48,19 +54,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
--- app/src/main/java/com/example/atish/accelero3/MainFragment.java
+++ app/src/main/java/com/example/atish/accelero3/MainFragment.java
@@ -1,7 +1,46 @@
 package com.example.atish.accelero3;
 
-/**
- * Created by Atish on 01/11/2015.
- */
-public class MainFragment {
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainFragment extends Fragment {
+
+    private static EditText topTextInpuText;
+    private static EditText bottomTextInput;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+            topTextInpuText = (EditText) view.findViewById(R.id.topTextInput);
+            bottomTextInput = (EditText) view.findViewById(R.id.bottomTextInput);
+            final Button fragment_button = (Button) view.findViewById(R.id.fragment_button);
+
+        fragment_button.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v) {
+                        Fragment_buttonClicked(v);
+
+                    }
+
+                }
+        );
+
+            return view;
+          }
+
+    public void Fragment_buttonClicked (View view){
+
+
+
+
+    }
 }
