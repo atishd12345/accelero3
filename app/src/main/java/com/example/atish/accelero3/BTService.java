c7ebfb1dede80eb0d1a0408076f0789085235a03
Atish <atishd12345@gmail.com>
2015-11-12 00:55:36 +0400

    Create BTService.java

---
+++ app/src/main/java/com/example/atish/accelero3/BTService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.github.amarinoembed.helloamarinoworld;
+
+import at.abraxas.amarino.service.AmarinoService;
+
+/**
+ * @author fbeachler
+ * 
+ */
+public class BTService extends AmarinoService {
+
+	public static final String TAG = "BTService";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see at.abraxas.amarino.service.AmarinoService#onCreate()
+	 */
+	@Override
+	public void onCreate() {
+		super.setIntentConfig(new ServiceIntentConfig());
+		//check if it needs to be MainActivity.class or MainFragment.class
+		super.setNotifLaunchIntentClass(MainActivity.class);
+		super.onCreate();
+	}
+
+}
